732c16f26e285c8ec2590173ec79593561071a10
adidarich <adidarich1994@gmail.com>
2023-06-17 20:28:34 +0500

    added urlpatterns for MEDIA_URL

--- app/urls.py
+++ app/urls.py
@@ -18,6 +18,9 @@ from django.contrib import admin
 from django.urls import path, include
 from django.contrib.sitemaps.views import sitemap
 from cd.sitemaps import PostSitemap
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 sitemaps = {
     'posts': PostSitemap,
@@ -27,5 +30,8 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('cd/', include('cd.urls', namespace='cd')),
     path('sitemap.xml', sitemap, {'sitemaps': sitemaps}, name='django.contrib.sitemaps.views.sitemap')
+
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
