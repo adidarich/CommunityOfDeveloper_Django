3ebada708548ec775476288a4550a22c70bec350
adidarich <adidarich1994@gmail.com>
2023-07-22 19:06:24 +0500

    added new paths logout and account/profile

--- registration/urls.py
+++ registration/urls.py
@@ -1,11 +1,12 @@
+from django.contrib.auth.views import LogoutView
 from django.urls import path, include
-from .views import SignUpView, SignInView
+from .views import SignUpView, SignInView, ProfileView
 
 app_name = 'registration'
 
 urlpatterns = [
     path('signup/', SignUpView.as_view(), name='signup'),
     path('signin/', SignInView.as_view(), name='signin'),
-    # path('logout/', name='signin'),
-    # path('account/profile', name='signin'),
+    path('logout/', LogoutView.as_view(), name='logout'),
+    path('account/profile', ProfileView.as_view(), name='profile'),
 ]
