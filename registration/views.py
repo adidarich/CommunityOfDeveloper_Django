2fb0ebf7da5f80b2367cde4a26a0d5c257cfdc45
adidarich <adidarich1994@gmail.com>
2023-07-22 19:06:47 +0500

    added class ProfileView

--- registration/views.py
+++ registration/views.py
@@ -1,7 +1,11 @@
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.models import User
 from django.contrib.auth.views import LoginView
+from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.views.generic import CreateView
+from django.views import View
 
 from .forms import SignUpForm, SignInForm
 
@@ -15,4 +19,13 @@ class SignUpView(CreateView):
 class SignInView(LoginView):
     form_class = SignInForm
     template_name = 'registration/signin.html'
+    success_url = reverse_lazy('cd:list')
 
+
+class ProfileView(LoginRequiredMixin, View):
+    template_name = 'registration/profile.html'
+    login_url = '/signin/'
+
+    def get(self, request: HttpRequest) -> HttpResponse:
+        user = User.objects.filter(username=request.user)[0]
+        return render(request, self.template_name, {'user': user})
